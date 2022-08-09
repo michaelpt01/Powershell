acad011a9ac89ee5d7a4789c556b52b131d2d50d
michaelpt01 <64911996+michaelpt01@users.noreply.github.com>
2022-08-09 09:11:32 -0400

    Update paramter_example.ps1

--- paramter_example.ps1
+++ paramter_example.ps1
@@ -1,3 +1,7 @@
 #the param keyword is used to define a parameter called $name.
 param($name = 'bub')
 "Hello $name, how are you?"
+
+#specify parameter this time
+.\hello Bruce
+Hello Bruce, how are you?
